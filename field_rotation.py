2d94666c2fe1ca5c1e905d032c772ab0043497d2
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-09-02 16:29:13 +1000

    Calculate alt az of planets based on geographic location

--- field_rotation.py
+++ field_rotation.py
@@ -1,22 +1,36 @@
 from skyfield.api import Loader, Topos
 
 
-# Download Ephemerides & timescale files
-load = Loader("ephemerides")
-planets = load("de421.bsp")
-ts = load.timescale()
-t = ts.now()
+def planet_vector(ephem, time, loc):
+    loc = earth + loc
+    astrometric = loc.at(time).observe(ephem)
+    apparent = astrometric.apparent()
+    alt, az, _ = apparent.altaz()
+    print(alt.dstr())
+    print(az.dstr())
 
 
-# Solar System Ephemerides
-sun = planets['sun']
-mercury = planets["mercury"]
-venus = planets["venus"]
-mars = planets["mars"]
-earth = planets['earth']
-moon = planets["moon"]
-jupiter = planets["jupiter barycenter"]
-saturn = planets["saturn barycenter"]
-uranus = planets["uranus barycenter"]
-neptune = planets["neptune barycenter"]
-pluto = planets["pluto barycenter"]
+if __name__ == "__main__":
+
+    # Download Ephemerides & timescale files
+    load = Loader("ephemerides")
+    planets = load("de421.bsp")
+    ts = load.timescale()
+
+    # Solar System Ephemerides
+    sun = planets["sun"]
+    mercury = planets["mercury"]
+    venus = planets["venus"]
+    mars = planets["mars"]
+    earth = planets["earth"]
+    moon = planets["moon"]
+    jupiter = planets["jupiter barycenter"]
+    saturn = planets["saturn barycenter"]
+    uranus = planets["uranus barycenter"]
+    neptune = planets["neptune barycenter"]
+    pluto = planets["pluto barycenter"]
+
+    t = ts.now()
+    Melbourne = Topos('37.8142 S', '144.9632 E', elevation_m=43)
+
+    planet_vector(jupiter, t, Melbourne)
