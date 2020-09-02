86f9b738256931d46275c02cf6357afca7ed2089
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-09-02 15:24:11 +1000

    Added basic solar system ephemeris stuff

--- field_rotation.py
+++ field_rotation.py
@@ -1,14 +1,22 @@
-from skyfield.api import Loader
+from skyfield.api import Loader, Topos
 
+
+# Download Ephemerides & timescale files
 load = Loader("ephemerides")
 planets = load("de421.bsp")
+ts = load.timescale()
+t = ts.now()
 
-mercury = planets["MERCURY"]
-venus = planets["VENUS"]
-mars = planets["MARS"]
-moon = planets["MOON"]
-jupiter = planets["JUPITER BARYCENTER"]
-saturn = planets["SATURN BARYCENTER"]
-uranus = planets["URANUS BARYCENTER"]
-neptune = planets["NEPTUNE BARYCENTER"]
-pluto = planets["PLUTO BARYCENTER"]
+
+# Solar System Ephemerides
+sun = planets['sun']
+mercury = planets["mercury"]
+venus = planets["venus"]
+mars = planets["mars"]
+earth = planets['earth']
+moon = planets["moon"]
+jupiter = planets["jupiter barycenter"]
+saturn = planets["saturn barycenter"]
+uranus = planets["uranus barycenter"]
+neptune = planets["neptune barycenter"]
+pluto = planets["pluto barycenter"]
